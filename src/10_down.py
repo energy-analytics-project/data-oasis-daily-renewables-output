89f61b826aa1b31f00f13c1d847f0647e1b89b46
Todd Greenwood-Geer <pub+github@zwrob.com>
2019-11-01 08:32:07 -0700

    fixed src/10_down.py
    downloads txt files, zips them, and copies orig txt file to /txt dir

--- src/10_down.py
+++ src/10_down.py
@@ -26,6 +26,7 @@
 #   an S3 bucket 'eap'.
 # -----------------------------------------------------------------------------
 
+import glob
 import datetime
 import requests
 import sys
@@ -33,10 +34,13 @@ import os
 import time
 import logging
 import json
-from edl.resources import state
+import shutil
+import zipfile
+from stat import S_IREAD, S_IRGRP, S_IROTH, S_IWRITE, S_IWGRP, S_IWOTH
 from edl.resources import log
-from edl.resources import web
+from edl.resources import state
 from edl.resources import time as xtime
+from edl.resources import web
 
 
 # -----------------------------------------------------------------------------
@@ -45,16 +49,18 @@ from edl.resources import time as xtime
 def config():
     """
     config = {
-            "source_dir"    : not used
-            "working_dir"   : location to download zip files
+            "source_dir"    : download .txt files here
+            "working_dir"   : zip .txt files and copy here 
             "state_file"    : fqpath to file that lists downloaded zip files
             }
     """
     cwd                     = os.path.abspath(os.path.curdir)
     zip_dir                 = os.path.join(cwd, "zip")
+    txt_dir                 = os.path.join(cwd, "txt")
     state_file              = os.path.join(zip_dir, "state.txt")
     config = {
             "working_dir"   : zip_dir,
+            "source_dir"    : txt_dir,
             "state_file"    : state_file
             }
     return config
@@ -68,6 +74,7 @@ def run(logger, manifest, config):
     resource_url    = manifest['url']
     delay           = manifest['download_delay_secs']
     download_dir    = config['working_dir']
+    txt_dir         = config['source_dir']
     state_file      = config['state_file']
     # sleep for N seconds in between downloads to meet caiso expected use requirements
     dates   = xtime.range_pairs(xtime.day_range_to_today(start_date))
@@ -83,17 +90,71 @@ def run(logger, manifest, config):
         "start_date": str(start_date),
         "urls_count": len(urls),
         })
-    state.update(
-            web.download(
-                logger,
-                resource_name,
-                delay,
-                urls,
-                state_file,
-                download_dir),
-            state_file
-            )
 
+    # download .txt files
+    downloaded_txt_urls = web.download(
+        logger,
+        resource_name,
+        delay,
+        urls,
+        state_file,
+        download_dir,
+        ending='.txt')
+
+    # copy .txt files to ./text dir and then
+    # compress original .txt files to .zip files
+    if not os.path.exists(txt_dir):
+        log.debug(logger, {
+            "name"      : __name__,
+            "method"    : "run",
+            "src"       : "10_down.py",
+            "message"   : "created target txt dir: %s" % txt_dir,
+            })
+        os.makedirs(txt_dir)
+
+    # process downloaded .txt files
+    data_files = glob.glob(os.path.join(download_dir, "*DailyRenewablesWatch.txt"))
+    for tf in data_files:
+        try:
+            # remove write protections for .txt files
+            os.chmod(os.path.join(download_dir, tf), S_IWRITE|S_IWGRP|S_IWOTH|S_IREAD|S_IRGRP|S_IROTH)
+            
+            # if the txt file is here, it needs to be copied to the ./txt dir
+            fqtf = os.path.join(download_dir, tf)
+            fqtf2 = os.path.join(txt_dir, tf)
+            fqtfzip = os.path.join(download_dir, '%s.zip' % tf)
+            if not os.path.exists(fqtf2):
+                shutil.copyfile(fqtf, fqtf2)
+            with zipfile.ZipFile(fqtfzip, 'w') as myzip:
+                myzip.write(fqtfzip)
+
+            # set .zip file to be read only
+            os.chmod(fqtfzip, S_IREAD|S_IRGRP|S_IROTH)
+        
+            # remove the zip/.txt file as it's been copied to txt/.txt
+            if os.path.exists(fqtf2) and os.path.exists(fqtfzip):
+                os.remove(fqtf)
+            log.debug(logger, {
+                "name"      : __name__,
+                "method"    : "run",
+                "src"       : "10_down.py",
+                "message"   : "zipped file: %s" % tf,
+                })
+        except Exception as e:
+            log.error(logger, {
+                "name"      : __name__,
+                "method"    : "run",
+                "src"       : "10_down.py",
+                "file"      : tf,
+                "error"     : "failed to process file",
+                "exception" : str(e),
+                })
+
+
+    # TODO: something is clobbering perms on the state file, so clobber it back
+    os.chmod(os.path.join(download_dir, 'state.txt'), S_IWRITE|S_IWGRP|S_IWOTH|S_IREAD|S_IRGRP|S_IROTH)
+    # final step
+    state.update(downloaded_txt_urls, state_file)
 # -----------------------------------------------------------------------------
 # Main
 # -----------------------------------------------------------------------------
